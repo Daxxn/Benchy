32846dd895b948456a998d11d871286b80f56ad8
Cody Lantz <46167982+Daxxn@users.noreply.github.com>
2022-06-05 11:34:44 -0700

    Initial PS functionality add block diagram

--- BenchyPowerSupply/src/main.cpp
+++ BenchyPowerSupply/src/main.cpp
@@ -1,20 +1,45 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "BenchyPowerSupplyPinout.h"
+#include "IntConverter.h"
+
+#define recieveBufferSize 16
+#define sendBufferSize 8
 
 const int FRONT_ADDR = 42;
 const int address = 32;
+
+uint8_t receiveBuffer[recieveBufferSize] = {};
+uint8_t sendBuffer[sendBufferSize] = {};
+uint8_t receiveCommand = 0;
+uint8_t sendCommand = 0;
 
 uint8_t lowSetVoltage = 0;
 uint8_t lowSetCurrent = 0;
 
 uint8_t highSet = 0;
 
-int V30Sense = 0;
-int V12Sense = 0;
-int highCCSense = 0;
+uint16_t V30Sense = 0;
+uint16_t V12Sense = 0;
+uint16_t highPSCurrent = 0;
+uint16_t highPSVoltage = 0;
+uint16_t lowPSCurrent = 0;
+uint16_t lowPSVoltage = 0;
 
 bool overCurrent = false;
+
+bool lowPSEnable = false;
+bool highPSEnable = false;
+
+// uint8_t GetLowByte(uint16_t value)
+// {
+//   return value & 0xFF;
+// }
+
+// uint8_t GetHighByte(uint16_t value)
+// {
+//   return (value & 0xFF00) >> 8;
+// }
 
 void HandleOverCurrent()
 {
@@ -31,23 +56,112 @@ void InitPins()
   );
 }
 
-void I2CRecieve(int param)
+#pragma region Main Controller Command functions
+void ResetCMD()
 {
+  lowPSEnable = false;
+  highPSEnable = false;
+  lowSetVoltage = 0;
+  lowSetCurrent = 0;
+  highSet = 0;
+}
+
+void LowPSSetVoltageCMD()
+{
+  lowSetVoltage = receiveBuffer[1];
+}
+
+void LowPSSetCurrentCMD()
+{
+  lowSetCurrent = receiveBuffer[1];
+}
+
+void HighPSSetVoltageCMD()
+{
+  highSet = receiveBuffer[1];
+}
+
+void SetPSStatesCMD()
+{
+  uint8_t val = receiveBuffer[1];
+  lowPSEnable = val & 0b01;
+  highPSEnable = val & 0b10;
+}
+
+int PSMonitorCMD()
+{
+  sendBuffer[8] = {};
+  sendBuffer[0] = GetLowByte(lowPSCurrent);
+  sendBuffer[1] = GetHighByte(lowPSCurrent);
+  sendBuffer[2] = GetLowByte(lowPSVoltage);
+  sendBuffer[3] = GetHighByte(lowPSVoltage);
+  sendBuffer[4] = GetLowByte(highPSCurrent);
+  sendBuffer[5] = GetHighByte(highPSCurrent);
+  sendBuffer[6] = GetLowByte(highPSVoltage);
+  sendBuffer[7] = GetHighByte(highPSVoltage);
   
+  return 8;
+}
+
+int PowerRailMonitorCMD()
+{
+  sendBuffer[4] = {};
+  sendBuffer[0] = GetLowByte(V30Sense);
+  sendBuffer[1] = GetHighByte(V30Sense);
+  sendBuffer[2] = GetLowByte(V12Sense);
+  sendBuffer[3] = GetHighByte(V12Sense);
+  return 4;
+}
+
+int StatusCMD()
+{
+  sendBuffer[1] = {};
+  uint8_t value = overCurrent;
+  value <<= 1;
+  value |= highPSEnable;
+  value <<= 1;
+  value |= lowPSEnable;
+  sendBuffer[0] = value;
+  return 1;
+}
+
+void (*receiveActions[])() = {
+  ResetCMD,
+  LowPSSetVoltageCMD,
+  LowPSSetCurrentCMD,
+  HighPSSetVoltageCMD,
+};
+
+int (*sendActions[])() = {
+  PSMonitorCMD,
+  PowerRailMonitorCMD,
+  StatusCMD,
+};
+#pragma endregion
+
+void I2CReceive(int len)
+{
+  if (len > 0)
+  {
+    Wire.readBytes(receiveBuffer, len);
+    receiveCommand = receiveBuffer[0];
+    receiveActions[receiveCommand]();
+  }
 }
 
 void I2CRequest()
 {
-  
+  sendCommand = Wire.read();
+  int size = sendActions[sendCommand]();
+  Wire.write(sendBuffer, size);
 }
 
 void setup() {
-  // Wire.begin(address);
-  // Wire.onReceive(I2CRecieve);
-  // Wire.onRequest(I2CRequest);
+  Wire.begin(address);
+  Wire.onReceive(I2CReceive);
+  Wire.onRequest(I2CRequest);
 
   //Initial Testing
-  Wire.begin();
   InitPins();
 }
 
@@ -56,7 +170,7 @@ void loop() {
   // Wire.write("Test");
   // Wire.endTransmission();
 
-  highSet++;
-  analogWrite(Digitl::PowerPins::HIGH_PWM_PIN, highSet);
+  // highSet++;
+  // analogWrite(Digitl::PowerPins::HIGH_PWM_PIN, highSet);
   delay(100);
 }
--- README.md
+++ README.md
@@ -14,6 +14,19 @@ Your electronics workbench buddy! (Is not actually your buddy, will probably try
 - Electronic load able to sink 5A
 - LCR Meter
 
-### I2C Bus
+## I2C Bus
 
 Im using a quite extensive I2C bus system to communicate with the different boards and functional blocks. This cuts WAY down on the number of wires and pins required for the main controller and allows for better modularity within benchy.
+
+## USB Hub
+
+The main reason why I needed to design my own USB hub instead of stuffing an off the shelf hub benchys trumpet was the absolute lack of protection circuitry in most "consumer grade" USB hubs. Theres no back-feed protection, verry little to no current monitoring, and the bare minimum ESD and EMI protection. Thats just not acceptable! imagine my supprise when I cracked open a $85 hub and found a couple controllers and thats it. The cost of finding a hub that has these protections is going to be around the same price as designing and building my own. (Except for the time investment...) Also, these features arent exaclty flashy, they dont look very good on a brochure. So they usually arent there.
+
+The most frustrating part is that these manufacturers _claim_ to conform to the USB spec when the lack protections doesnt.
+
+## Power Supply
+
+The one thing that made the two different power supplies possible was the Switch-Mode Power Supply (SMPS) that was built into the DVR box that spawned this project. The output from the SMPS is a high-current 12V source and a 30V source.
+
+### Low-Voltage Supply
+
